4d2b38fdf645c273f75d44e837d34418b51d52d5
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2023-12-30 17:35:09 +0100

    Measure of Locations
    1. The following observations are given:
    1,2,3,3,2,45,45,56,67,55,67,56,67,68,55,54,43,32,22,33
    (a) Calculate the arithmethic mean!
    (b) Calculate median!
    (c) Determine the mode!

---
+++ mean median mode Ida.R
@@ -0,0 +1,13 @@
+#1. The following observations are given:
+#1,2,3,3,2,45,45,56,67,55,67,56,67,68,55,54,43,32,22,33
+
+#(a) Calculate the arithmethic mean!
+#(b) Calculate median!
+#(c) Determine the mode!
+
+
+x=c(1,2,3,3,2,45,45,56,67,55,67,56,67,68,55,54,43,32,22,33)
+mean(x)  #38.8
+median(x) #45
+
+table(x)  #67
